629a174698f832b15109d5fcf9cc272ba43417d3
Edaa44 <eda.erkek.ch@gmail.com>
2026-03-18 13:57:06 +0100

    update prob.py file

--- Prob.py
+++ Prob.py
@@ -1,1 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
+sensitivity = 0.99
+
+prevalence = np.linspace(0.00001, 0.5, 1000)
+
+specificities = [0.99, 0.999, 0.9999, 0.99999]
+
+def posterior_prob(p, sensitivity, specificities):
+    return (sensitivity * p) / (sensitivity * p + (1 - specificities) * (1 - p))
+
+plt.figure(figsize=(10, 6))
+
+for spec in specificities:
+    prob = posterior_prob(prevalence, sensitivity, spec)
+    plt.plot(prevalence * 100, prob * 100, label=f"Specificity = {spec*100:.3f}%")
+
+plt.xlabel("Prevalence in %")
+plt.ylabel("P(Infect|Pos) in %")
+plt.grid(True)
+
+plt.show()
